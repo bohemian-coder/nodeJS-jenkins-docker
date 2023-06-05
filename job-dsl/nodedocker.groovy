29d806cd4148ad878f5173337940cda0909333e1
bohemian-coder <jenniferejeme@gmail.com>
2023-06-05 17:43:10 -0500

    added node to docker groovy file

---
+++ job-dsl/nodedocker.groovy
@@ -0,0 +1,25 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/bohemian-coder/nodeJS-jenkins-docker.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('bohemian-coder')
+            node / gitConfigEmail('jenniferejeme@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') 
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('maybellemo/node-jenkins-docker')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockercredentials')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
